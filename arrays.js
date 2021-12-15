29ebb72c75eea9d34488a1d6d40fe3fb29b02339
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-15 22:52:31 +0530

    UPDATED

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var array = ["1", "2", "3", "4"];
+console.log(array[2])
+
+array.push('four');
+console.log(array)
+
+array.unshift('-1');
+console.log(array)
+
+array[5]='five';
+console.log(array)
+
+array.pop('0');
+console.log(array)
+
+console.log(array.length);
